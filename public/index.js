b0cdee76adf97f174809683c7a2c1f082b572746
github username <github email address>
2019-12-16 15:32:11 -0500

    Added products section, with some styling.

--- public/index.js
+++ public/index.js
@@ -9,5 +9,29 @@ function init() {
 function fetchProducts() {
   fetch(baseUrl)
     .then(res => res.json())
-    .then(console.log)
+    .then(products => renderProducts(products))
+}
+
+function renderProducts(products) {
+  let productsContainer = document.querySelector(".products-container")
+
+  products.map((product) => {
+    let { Code, Price } = product
+
+    let div = document.createElement("div")
+    div.className = "product-item"
+
+    let productCode = document.createElement("p")
+    productCode.className = "product-property"
+
+    let productPrice = document.createElement("p")
+    productPrice.className = "product-property" 
+    
+    productCode.innerText = `Code: ${Code}`
+    productPrice.innerText = `Price: $${Price * 0.01}`
+
+    div.appendChild(productCode)
+    div.appendChild(productPrice)
+    productsContainer.appendChild(div)
+  })
 }
